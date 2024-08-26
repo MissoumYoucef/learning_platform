042bb40f7ab6c1005aa8a235cf078d1eaa3c749b
MissoumYoucef <Missoumleetcoding@gmail.com>
2024-08-26 21:02:07 +0100

    .

--- src/components/overview/card.jsx
+++ src/components/overview/card.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './card.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'; // Example icon
 
 function Card() {
   return (
     <div className='card_container'>
-      <p className='card_title'> Course in progress</p>
+      <div className='card_title'>
+        <FontAwesomeIcon icon={faGraduationCap} />
+        <p> Course in progress</p>
+      </div>
       <p className='card_number'>3</p>
     </div>
   );
---
+++ src/components/overview/live_events.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './live_events.css';
+import liveev from "./images/liveev.png"
+
+function LiveEvents() {
+  return (
+    <div className='live_events_part'>
+        <div className='live_events_container'>
+            <div>
+                <p className='live_events_text'>Ipsum odio et 
+                    integer aliquet lorem a,
+                     sem suscipit varius.</p>
+                <div>
+                    <p>Missoum youcef</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+
+export default LiveEvents;
